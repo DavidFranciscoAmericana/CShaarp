8ba4bb62be5f54f222272424ea15799b0e36d286
DavidFranciscoAmericana <davidapp.df@gmail.com>
2023-07-02 16:48:27 -0300

    v1

---
+++ URI1078/URI1078/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace URI1078 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            number = int.Parse(Console.ReadLine());
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(number+" x " + i +" = " + number*i);
+            }
+
+        }
+    }
+}
