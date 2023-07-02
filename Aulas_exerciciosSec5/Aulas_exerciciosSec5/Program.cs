992ab9ece6141624d58b20efe72885a2f9b68e0f
DavidFranciscoAmericana <davidapp.df@gmail.com>
2023-07-02 14:38:59 -0300

    v1

--- Aulas_exerciciosSec5/Aulas_exerciciosSec5/Program.cs
+++ Aulas_exerciciosSec5/Aulas_exerciciosSec5/Program.cs
@@ -6,7 +6,15 @@ namespace ForRepeticao // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-           
+            int n, number, total;
+            total = 0;
+            n = int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                number = int.Parse(Console.ReadLine());
+                total += number;
+            }
+            Console.WriteLine(total);
         }
     }
 }
