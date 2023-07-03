e9e64e1edaae45bef2bdfffbefb47557cf1b3e2d
DavidFranciscoAmericana <davidapp.df@gmail.com>
2023-07-03 14:47:26 -0300

    v1

---
+++ Vetores/Vetores/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Vetores // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Criação de vetores
+            string[] nomes;
+            int[] idades;
+            double[] peso;
+
+            //Instanciando os vetores
+            nomes = new string[5]; 
+            idades = new int[5];
+            peso = new double[5];
+
+            //Acessando vetores
+            nomes[0] = "David";
+            idades[0] = 44;
+            peso[0] = 88.2;
+
+            //Escreve no console
+            Console.WriteLine(nomes[0]+" " + idades[0] + " " + peso[0]);
+
+        }
+    }
+}
