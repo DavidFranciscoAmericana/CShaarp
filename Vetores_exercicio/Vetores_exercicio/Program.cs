212564897a8441ebd3451fdd20d224910f48b962
DavidFranciscoAmericana <davidapp.df@gmail.com>
2023-07-03 16:13:51 -0300

    v1

---
+++ Vetores_exercicio/Vetores_exercicio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores_Exercicio // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int[] vet;
+
+            n = int.Parse(Console.ReadLine());
+            vet = new int[n];
+            string[] entrada = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = int.Parse(entrada[i]);
+            }
+            //imprime cada posição do vetor
+            for (int i = 0; i < n; i++)
+            {
+                if (vet[i] < 0)
+                {
+                    Console.WriteLine(vet[i].ToString());
+                }
+                
+            }
+        }
+    }
+}
