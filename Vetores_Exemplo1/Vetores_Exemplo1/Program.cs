1fe58a147b3aca42f74d21e8ad094cd012f7145b
DavidFranciscoAmericana <davidapp.df@gmail.com>
2023-07-03 14:59:05 -0300

    v1

---
+++ Vetores_Exemplo1/Vetores_Exemplo1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores_Exemplo1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            double[] vet;
+
+            n = int.Parse(Console.ReadLine());
+            vet = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            }
+            //imprime cada posição do vetor
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine(vet[i].ToString("F1",CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
